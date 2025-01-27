68153c31c7924c0c739f07c02e13cf722417b733
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2025-01-27 13:28:20 +0300

    animation for menu for small screens done

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
+
 
 const Header = () : React.ReactElement => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const barStyle = 'w-5 h-[3px] bg-black mt-1 transition-transform';
+  const barStyle1 = 'w-5 h-[3px] bg-black mt-[6px] transition-transform rotate-[135deg] translate-y-[4px]';
+  const barStyle2 = 'transition-opacity opacity-0';
+  const barStyle3 = 'w-5 h-[3px] bg-black mt-[6px] transition-transform -rotate-[135deg] -translate-y-[5px]';
+
   return(
-    <div className="min-h-[15%] flex items-center justify-between p-5">
-      <span className="font-serif text-indigo-800 text-4xl lg:text-6xl">I B J</span>
-      <ul className="list-none flex gap-x-5 tracking-wide ">
+    <div className="min-h-[15%] flex items-center justify-between md:justify-around p-5">
+      <span className="font-serif text-indigo-800 text-4xl md:text-5xl lg:text-6xl min-w-24 cursor-pointer">I B J</span>
+
+      {/* Menu for large screens */}
+      <ul className="hidden sm:flex list-none gap-x-5 tracking-wide text-sm md:text-base lg:text-lg">
         <li className="rounded-sm cursor-pointer border-b-2 border-transparent duration-200 hover:border-indigo-700 hover:opacity-80">HOME</li>
         <li className="rounded-sm cursor-pointer border-b-2 border-transparent duration-200 hover:border-indigo-700 hover:opacity-80">ABOUT US</li>
         <li className="rounded-sm cursor-pointer border-b-2 border-transparent duration-200 hover:border-indigo-700 hover:opacity-80">OUR SERVICES</li>
         <li className="rounded-sm cursor-pointer border-b-2 border-transparent duration-200 hover:border-indigo-700 hover:opacity-80">OUR PROJECTS</li>
         <li className="rounded-sm cursor-pointer border-b-2 border-transparent duration-200 hover:border-indigo-700 hover:opacity-80">CONTACT US</li>
       </ul>
+
+      {/* Menu for small screens */}
+      <div className="inline-block sm:hidden transition-transform cursor-pointer" onClick={()=>setToggleMenu(!toggleMenu)}>
+        <div className={toggleMenu ? barStyle1: barStyle}></div>
+        <div className={toggleMenu ? barStyle2: barStyle}></div>
+        <div className={toggleMenu ? barStyle3: barStyle}></div>
+      </div>
+
     </div>
   )
 }
