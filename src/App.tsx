350e3ef047c06a27dd6a61940b57898ac2eae48e
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2025-01-27 08:45:18 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 const App = () => {
   return (
     <div className="min-h-screen">
-      <div className="min-h-screen bg-red-500">a</div>
-      <div className="bg-indigo-500">a</div>
+      <div className="h-[100vh] w-full">
+        <div className="min-h-[15%] bg-red-500">a</div>
+        <div className="bg-indigo-500">a</div>
+      </div>
     </div>
   )
 }
