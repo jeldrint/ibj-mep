04c4ac4b78d10c5ba6be419db912e98e2d680955
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2025-02-16 08:50:37 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import NavSmall from "./components/sub-components/NavSmall";
+// import NavSmall from "./components/sub-components/NavSmall";
 
 export type ToggleDropDownProps = {
    aboutus: boolean,
