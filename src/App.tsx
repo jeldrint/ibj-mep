d7a75d7f349a8c609195a40d9211e14aebc2005e
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2025-02-16 10:31:02 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import NavSmall from "./components/sub-components/NavSmall";
+//import NavSmall from "./components/sub-components/NavSmall";
 // import { useState } from "react";
 
 const App = (): React.ReactElement => {
@@ -9,7 +9,7 @@ const App = (): React.ReactElement => {
 
    return (
       <div className="min-h-screen max-w-screen font-poppins bg-slate-100 flex flex-col justify-between overflow-hidden">
-         <NavSmall />
+         {/* <NavSmall /> */}
          <Header />
          <Outlet />
          <Footer />
