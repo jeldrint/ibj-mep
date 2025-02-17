91fde6be8275e55122809e2e399e7bacf78dd3f0
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2025-02-17 19:44:10 +0300

    Update PersonnelProfile.tsx

--- src/components/sub-components/PersonnelProfile.tsx
+++ src/components/sub-components/PersonnelProfile.tsx
@@ -9,7 +9,7 @@ const PersonnelProfile = ({name, title, img, desc1, desc2} : PersonnelProps) : R
    return(
       <div ref={ref} className='my-[50px] xl:my-[100px]'>
          <div className={`${isInViewport ? 'animate-in duration-1.5s fade-in-5 slide-in-from-top-20' : 'invisible'}`}>
-            <img className={`float-left mr-5 h-[20vh] sm:h-[30vh] rounded-md opacity-80`} src={img} alt='ibrahim-bin-jibreen' />
+            <img className={`float-left mr-5 h-[20vh] sm:h-[30vh] rounded-md opacity-80`} src={img} alt={name.replace(/\.+/g, '').replace(/\s+/g, '-').toLowerCase()} />
             <span className='text-lg font-bold'>{name} &mdash;</span>
             <span className='text-base font-bold'>{title}</span>
          </div>
