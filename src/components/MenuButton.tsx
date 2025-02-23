edb6d2b7d616627765b765e38937b92d3c56a6c7
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2025-02-23 23:39:33 +0300

    Update MenuButton.tsx

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -5,7 +5,7 @@ const MenuButton = ({toggleMenu, setToggleMenu}: ToggleMenuProps): React.ReactEl
    const barStyle = 'w-[21px] h-[2.5px] bg-indigo-800 my-[3.5px] transition-transform';
    const barStyle1 = 'w-[21px] h-[2.5px] bg-indigo-800 my-[5.1px] transition-transform rotate-[135deg] translate-y-[3.2px]';
    const barStyle2 = 'transition-opacity opacity-0';
-   const barStyle3 = 'w-[21px] h-[2.5px] bg-indigo-800 my-[5.1px] transition-transform -rotate-[135deg] -translate-y-[4px]';
+   const barStyle3 = 'w-[21px] h-[2.5px] bg-indigo-800 my-[5.1px] transition-transform -rotate-[135deg] -translate-y-[4.2px]';
 
    return (
       <div className="z-20 fixed top-[45px] right-2 inline-block sm:hidden transition-transform cursor-pointer p-3 bg-indigo-500 rounded-full" onClick={()=>setToggleMenu(!toggleMenu)}>
