4cf1d44de59ad4fe0cda726d3f2ac2b16ad83a2a
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2025-02-24 10:05:25 +0300

    nav for small screens now has a smooth dropdown

--- src/components/sub-components/Dropdown.tsx
+++ src/components/sub-components/Dropdown.tsx
@@ -11,6 +11,7 @@ type DropdownProps = {
 
 const DropDown = ({navName, toggleDropdown, setToggleDropdown, newNavName, dropdownItems}: DropdownProps) => {
 
+   if(screen.width > 640){
       return(
          <div onMouseEnter={()=>setToggleDropdown({...toggleDropdown, [newNavName]: true})} onMouseLeave={()=>setToggleDropdown({...toggleDropdown, [newNavName]: false})}
             className={`${!toggleDropdown[newNavName as keyof typeof toggleDropdown] ? 'invisible' : 'visible' } absolute z-10 p-3 rounded flex flex-col cursor-pointer text-right sm:text-left text-xs sm:text-base bg-slate-400 text-slate-50`}>
@@ -21,7 +22,21 @@ const DropDown = ({navName, toggleDropdown, setToggleDropdown, newNavName, dropd
                })
             }
          </div>
-      )   
+      )
+
+   // ** Dropdown for the smaller screens
+   }else{
+      return(
+         <div className={`${!toggleDropdown[newNavName as keyof typeof toggleDropdown] ? 'hidden' : 'flex animate-in fade-in-5 slide-in-from-top-7' } flex-col duration-1000 px-2 rounded cursor-pointer text-right sm:text-left text-xs sm:text-base bg-transparent text-slate-50`}>
+            {
+               dropdownItems.map((item, idx)=>{
+                  const path: string = item.replace(/\s+/g, '-').toLowerCase();
+                  return <Link to={`/${navName}/${path}`} key={idx} reloadDocument={true} className="transition-colors active:text-indigo-800">{item}</Link>
+               })
+            }
+         </div>
+      )      
    }
+}
 
 export default DropDown;
--- src/components/sub-components/NavItemsSmall.tsx
+++ src/components/sub-components/NavItemsSmall.tsx
@@ -21,7 +21,7 @@ const NavItemsSmall = ({navName, withDropdown, dropdownItems}: HeaderNavProps) :
    if(withDropdown){
       return(
          <div>
-            <div onMouseLeave={()=>setToggleDropdown({...toggleDropdown, [newNavName]: false})} className='text-sm flex items-start rounded-sm cursor-pointer border-2 p-2 border-transparent' >
+            <div onMouseLeave={()=>setToggleDropdown({...toggleDropdown, [newNavName]: false})} className='text-sm flex items-start justify-end rounded-sm cursor-pointer p-2' >
                <Link to={`/${navName}`} reloadDocument={true}>{navName.replace(/-/g,' ').toUpperCase()}&ensp;</Link>
                <img alt='arrow-down' src={arrowIcon} onClick={handleClick} className="w-4 lg:w-5 hover:animate-[spin_0.5s_ease-in-out]" />
             </div>
@@ -30,7 +30,7 @@ const NavItemsSmall = ({navName, withDropdown, dropdownItems}: HeaderNavProps) :
 
       )
    }else{
-      return <Link to='/' reloadDocument={true} className={`rounded-sm cursor-pointer border-2 p-2 border-transparent text-sm`} >{navName.replace(/-/g,' ').toUpperCase()}</Link>
+      return <Link to='/' reloadDocument={true} className={`rounded-sm cursor-pointer p-3 text-sm`} >{navName.replace(/-/g,' ').toUpperCase()}</Link>
    }
 }
 
