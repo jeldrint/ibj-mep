18f960c35a7a63193d80db27f5f7735ace02ae55
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2025-04-02 09:50:56 +0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -31,8 +31,8 @@ const About = () : React.ReactElement => {
          {/* ABOUT Footer  */}
          <section ref={ref4} className={`${isInViewport4 ? 'animate-in duration-1.5s fade-in-5 slide-in-from-bottom-20' : 'invisible'} relative h-[40vh] bg-cover bg-bottom lg:bg-[0%_85%] w-full bg-blue-800 bg-blend-hard-light`} style={{backgroundImage: `url(${pic2})`}}>
             <div className='opacity-85 absolute p-3 md:p-9 h-full w-full flex flex-col gap-y-2 items-center justify-center backdrop-blur-sm'>
-               <span className='text-xl sm:text-4xl md:text-6xl  text-indigo-800 bg-slate-100 p-2 rounded-md'>Subscribe to our Newsletter</span>
-               <span className='text-sm md:text-base lg:text-lg text-black text-center max-w-[900px] bg-slate-100 p-[1px] rounded-md'>By subscribing to the IBJ newsletter, you'll receive regular updates, news, and job openings delivered straight to your inbox.</span>
+               <span className='text-xl sm:text-4xl md:text-6xl text-center text-indigo-800 bg-slate-100 p-2 rounded-md'>Subscribe to our Newsletter</span>
+               <span className='text-sm md:text-base lg:text-lg text-center text-black max-w-[900px] bg-slate-100 p-[1px] rounded-md'>By subscribing to the IBJ newsletter, you'll receive regular updates, news, and job openings delivered straight to your inbox.</span>
                <form className=''>
                   <input className='text-sm md:text-base min-w-[150px] md:min-w-[350px] p-3 rounded-md' name='email' id='email' placeholder='Enter your E-mail Address' type='email' required />
                   <button type='submit' className='p-3 rounded-md text-sm md:text-base  bg-indigo-700 active:bg-indigo-900'>SUBMIT</button>
